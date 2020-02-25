f7ba15f6931dbb0b2e477dcec462d41a50fa8a26
Can <mca199667@gmail.com>
2020-02-25 16:15:58 +0300

    Each lecture pages

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-const Header = ({ title }) => {
+const Header = ({
+  title,
+  imgPath = "url(./img/header-bg.jpg) no-repeat center center/cover"
+}) => {
   return (
-    <div
-      className="header"
-      style={{
-        background: "url(./img/header-bg.jpg) no-repeat center center/cover"
-      }}
-    >
+    <div className="header" style={{ background: imgPath }}>
       {title}
     </div>
   );
--- src/components/pages/lectures/Physics.js
+++ src/components/pages/lectures/Physics.js
@@ -1,7 +1,102 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Header from "../../layout/Header";
 
 const Physics = () => {
-  return <div>Physics page</div>;
+  return (
+    <div className="physics-page">
+      <Header
+        title={"Physics"}
+        imgPath={
+          "url(./img/physics-header-bg.jpg) no-repeat center center/cover"
+        }
+      />
+      <div className="navigation">
+        <Link to="/">Home</Link>&nbsp;&middot;&nbsp;
+        <Link to="/lectures">Lectures</Link>&nbsp;&middot;&nbsp;
+        <Link to="/physics">Physics</Link>
+      </div>
+      <div className="page-wrapper">
+        <h1 className="page-title">Lorem ipsum dolor sit.</h1>
+        <p className="page-paragraph">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+          error voluptas dolorum quod. Deserunt, error. Ipsa sed architecto,
+          iure totam expedita doloremque laboriosam similique omnis pariatur
+          iste numquam, voluptas quasi quia, eligendi impedit nemo sint
+          asperiores! Illo debitis vitae cupiditate iure pariatur esse quibusdam
+          dolores, dolor numquam dignissimos! Totam repudiandae iure eos eaque
+          quasi dolor quibusdam vel molestiae distinctio in.
+        </p>
+        <hr />
+        <div className="page-cards-wrapper">
+          <div className="page-card" style={{ backgroundColor: "pink" }}>
+            <div className="page-card-img">
+              <i className="fas fa-chalkboard"></i>
+            </div>
+            <h3 className="page-card-title">Lorem, ipsum.</h3>
+            <p className="page-card-body">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+          <div className="page-card" style={{ backgroundColor: "skyblue" }}>
+            <div className="page-card-img">
+              <i className="fas fa-book-open"></i>
+            </div>
+            <h3 className="page-card-title">Lorem, ipsum.</h3>
+            <p className="page-card-body">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+          <div className="page-card" style={{ backgroundColor: "olive" }}>
+            <div className="page-card-img">
+              <i className="fas fa-school"></i>
+            </div>
+            <h3 className="page-card-title">Lorem, ipsum.</h3>
+            <p className="page-card-body">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+          <div className="page-card" style={{ backgroundColor: "yellow" }}>
+            <div className="page-card-img">
+              <i className="fas fa-microscope"></i>
+            </div>
+            <h3 className="page-card-title">Lorem, ipsum.</h3>
+            <p className="page-card-body">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+        <hr />
+        <h1 className="page-title">Lorem, ipsum.</h1>
+        <ul className="page-list">
+          <li>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, ea!
+          </li>
+          <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+          <li>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed atque
+            veniam saepe.
+          </li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, ea!
+          </li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, ea!
+          </li>
+          <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+          <li>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed atque
+            veniam saepe.
+          </li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, ea!
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Physics;
