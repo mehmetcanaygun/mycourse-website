3d28ca4effcec0e8a9c6b2f6f63e5f92d30d57db
Can <mca199667@gmail.com>
2020-02-25 18:28:16 +0300

    Tutors page

--- src/components/pages/Classrooms.js
+++ src/components/pages/Classrooms.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Header from "../layout/Header";
 
 const Classrooms = () => {
-  return <div>Classrooms page</div>;
+  return (
+    <div className="classrooms-page">
+      <Header title={"Classrooms"} />
+      <div className="navigation">
+        <Link to="/">Home</Link>&nbsp;&middot;&nbsp;
+        <Link to="/classrooms">Classrooms</Link>
+      </div>
+      <div className="page-wrapper">asd</div>
+    </div>
+  );
 };
 
 export default Classrooms;
--- src/components/pages/Tutors.js
+++ src/components/pages/Tutors.js
@@ -1,7 +1,415 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Header from "../layout/Header";
 
 const Tutors = () => {
-  return <div>Tutors page</div>;
+  return (
+    <div className="tutors-page">
+      <Header title={"Tutors"} />
+      <div className="navigation">
+        <Link to="/">Home</Link>&nbsp;&middot;&nbsp;
+        <Link to="/tutors">Tutors</Link>
+      </div>
+      <div className="page-wrapper">
+        {/* MATHS DEPARTMENT START */}
+        <h1 className="page-title">Maths Department</h1>
+        <div className="tutors-page__tutors">
+          <div className="tutor">
+            <div className="tutor-img">
+              <img src="./img/tutor-avatar.png" alt="tutor" width="150" />
+            </div>
+            <ul className="tutor-info">
+              <li>
+                <strong>Name Surname</strong>: John Doe
+              </li>
+              <li>
+                <strong>Phone</strong>: 0123 456 78 90
+              </li>
+              <li>
+                <strong>Mail</strong>: jdoe@mycourse.com
+              </li>
+              <li>
+                <strong>Office</strong>: A-215
+              </li>
+            </ul>
+          </div>
+          <div className="tutor">
+            <div className="tutor-img">
+              <img src="./img/tutor-avatar.png" alt="tutor" width="150" />
+            </div>
+            <ul className="tutor-info">
+              <li>
+                <strong>Name Surname</strong>: John Doe
+              </li>
+              <li>
+                <strong>Phone</strong>: 0123 456 78 90
+              </li>
+              <li>
+                <strong>Mail</strong>: jdoe@mycourse.com
+              </li>
+              <li>
+                <strong>Office</strong>: A-215
+              </li>
+            </ul>
+          </div>
+          <div className="tutor">
+            <div className="tutor-img">
+              <img src="./img/tutor-avatar.png" alt="tutor" width="150" />
+            </div>
+            <ul className="tutor-info">
+              <li>
+                <strong>Name Surname</strong>: John Doe
+              </li>
+              <li>
+                <strong>Phone</strong>: 0123 456 78 90
+              </li>
+              <li>
+                <strong>Mail</strong>: jdoe@mycourse.com
+              </li>
+              <li>
+                <strong>Office</strong>: A-215
+              </li>
+            </ul>
+          </div>
+          <div className="tutor">
+            <div className="tutor-img">
+              <img src="./img/tutor-avatar.png" alt="tutor" width="150" />
+            </div>
+            <ul className="tutor-info">
+              <li>
+                <strong>Name Surname</strong>: John Doe
+              </li>
+              <li>
+                <strong>Phone</strong>: 0123 456 78 90
+              </li>
+              <li>
+                <strong>Mail</strong>: jdoe@mycourse.com
+              </li>
+              <li>
+                <strong>Office</strong>: A-215
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* MATHS DEPARTMENT END */}
+
+        {/* PHYSICS DEPARTMENT START */}
+        <h1 className="page-title">Physics Department</h1>
+        <div className="tutors-page__tutors">
+          <div className="tutor">
+            <div className="tutor-img">
+              <img src="./img/tutor-avatar.png" alt="tutor" width="150" />
+            </div>
+            <ul className="tutor-info">
+              <li>
+                <strong>Name Surname</strong>: John Doe
+              </li>
+              <li>
+                <strong>Phone</strong>: 0123 456 78 90
+              </li>
+              <li>
+                <strong>Mail</strong>: jdoe@mycourse.com
+              </li>
+              <li>
+                <strong>Office</strong>: A-215
+              </li>
+            </ul>
+          </div>
+          <div className="tutor">
+            <div className="tutor-img">
+              <img src="./img/tutor-avatar.png" alt="tutor" width="150" />
+            </div>
+            <ul className="tutor-info">
+              <li>
+                <strong>Name Surname</strong>: John Doe
+              </li>
+              <li>
+                <strong>Phone</strong>: 0123 456 78 90
+              </li>
+              <li>
+                <strong>Mail</strong>: jdoe@mycourse.com
+              </li>
+              <li>
+                <strong>Office</strong>: A-215
+              </li>
+            </ul>
+          </div>
+          <div className="tutor">
+            <div className="tutor-img">
+              <img src="./img/tutor-avatar.png" alt="tutor" width="150" />
+            </div>
+            <ul className="tutor-info">
+              <li>
+                <strong>Name Surname</strong>: John Doe
+              </li>
+              <li>
+                <strong>Phone</strong>: 0123 456 78 90
+              </li>
+              <li>
+                <strong>Mail</strong>: jdoe@mycourse.com
+              </li>
+              <li>
+                <strong>Office</strong>: A-215
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* PHYSICS DEPARTMENT END */}
+
+        {/* CHEMISTRY DEPARTMENT START */}
+        <h1 className="page-title">Chemistry Department</h1>
+        <div className="tutors-page__tutors">
+          <div className="tutor">
+            <div className="tutor-img">
+              <img src="./img/tutor-avatar.png" alt="tutor" width="150" />
+            </div>
+            <ul className="tutor-info">
+              <li>
+                <strong>Name Surname</strong>: John Doe
+              </li>
+              <li>
+                <strong>Phone</strong>: 0123 456 78 90
+              </li>
+              <li>
+                <strong>Mail</strong>: jdoe@mycourse.com
+              </li>
+              <li>
+                <strong>Office</strong>: A-215
+              </li>
+            </ul>
+          </div>
+          <div className="tutor">
+            <div className="tutor-img">
+              <img src="./img/tutor-avatar.png" alt="tutor" width="150" />
+            </div>
+            <ul className="tutor-info">
+              <li>
+                <strong>Name Surname</strong>: John Doe
+              </li>
+              <li>
+                <strong>Phone</strong>: 0123 456 78 90
+              </li>
+              <li>
+                <strong>Mail</strong>: jdoe@mycourse.com
+              </li>
+              <li>
+                <strong>Office</strong>: A-215
+              </li>
+            </ul>
+          </div>
+          <div className="tutor">
+            <div className="tutor-img">
+              <img src="./img/tutor-avatar.png" alt="tutor" width="150" />
+            </div>
+            <ul className="tutor-info">
+              <li>
+                <strong>Name Surname</strong>: John Doe
+              </li>
+              <li>
+                <strong>Phone</strong>: 0123 456 78 90
+              </li>
+              <li>
+                <strong>Mail</strong>: jdoe@mycourse.com
+              </li>
+              <li>
+                <strong>Office</strong>: A-215
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* CHEMISTRY DEPARTMENT END */}
+
+        {/* BIOLOGY DEPARTMENT START */}
+        <h1 className="page-title">Biology Department</h1>
+        <div className="tutors-page__tutors">
+          <div className="tutor">
+            <div className="tutor-img">
+              <img src="./img/tutor-avatar.png" alt="tutor" width="150" />
+            </div>
+            <ul className="tutor-info">
+              <li>
+                <strong>Name Surname</strong>: John Doe
+              </li>
+              <li>
+                <strong>Phone</strong>: 0123 456 78 90
+              </li>
+              <li>
+                <strong>Mail</strong>: jdoe@mycourse.com
+              </li>
+              <li>
+                <strong>Office</strong>: A-215
+              </li>
+            </ul>
+          </div>
+          <div className="tutor">
+            <div className="tutor-img">
+              <img src="./img/tutor-avatar.png" alt="tutor" width="150" />
+            </div>
+            <ul className="tutor-info">
+              <li>
+                <strong>Name Surname</strong>: John Doe
+              </li>
+              <li>
+                <strong>Phone</strong>: 0123 456 78 90
+              </li>
+              <li>
+                <strong>Mail</strong>: jdoe@mycourse.com
+              </li>
+              <li>
+                <strong>Office</strong>: A-215
+              </li>
+            </ul>
+          </div>
+          <div className="tutor">
+            <div className="tutor-img">
+              <img src="./img/tutor-avatar.png" alt="tutor" width="150" />
+            </div>
+            <ul className="tutor-info">
+              <li>
+                <strong>Name Surname</strong>: John Doe
+              </li>
+              <li>
+                <strong>Phone</strong>: 0123 456 78 90
+              </li>
+              <li>
+                <strong>Mail</strong>: jdoe@mycourse.com
+              </li>
+              <li>
+                <strong>Office</strong>: A-215
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* BIOLOGY DEPARTMENT END */}
+
+        {/* LITERATURE DEPARTMENT START */}
+        <h1 className="page-title">Literature Department</h1>
+        <div className="tutors-page__tutors">
+          <div className="tutor">
+            <div className="tutor-img">
+              <img src="./img/tutor-avatar.png" alt="tutor" width="150" />
+            </div>
+            <ul className="tutor-info">
+              <li>
+                <strong>Name Surname</strong>: John Doe
+              </li>
+              <li>
+                <strong>Phone</strong>: 0123 456 78 90
+              </li>
+              <li>
+                <strong>Mail</strong>: jdoe@mycourse.com
+              </li>
+              <li>
+                <strong>Office</strong>: A-215
+              </li>
+            </ul>
+          </div>
+          <div className="tutor">
+            <div className="tutor-img">
+              <img src="./img/tutor-avatar.png" alt="tutor" width="150" />
+            </div>
+            <ul className="tutor-info">
+              <li>
+                <strong>Name Surname</strong>: John Doe
+              </li>
+              <li>
+                <strong>Phone</strong>: 0123 456 78 90
+              </li>
+              <li>
+                <strong>Mail</strong>: jdoe@mycourse.com
+              </li>
+              <li>
+                <strong>Office</strong>: A-215
+              </li>
+            </ul>
+          </div>
+          <div className="tutor">
+            <div className="tutor-img">
+              <img src="./img/tutor-avatar.png" alt="tutor" width="150" />
+            </div>
+            <ul className="tutor-info">
+              <li>
+                <strong>Name Surname</strong>: John Doe
+              </li>
+              <li>
+                <strong>Phone</strong>: 0123 456 78 90
+              </li>
+              <li>
+                <strong>Mail</strong>: jdoe@mycourse.com
+              </li>
+              <li>
+                <strong>Office</strong>: A-215
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* LITERATURE DEPARTMENT END */}
+
+        {/* GEOGRAPHY DEPARTMENT START */}
+        <h1 className="page-title">Geography Department</h1>
+        <div className="tutors-page__tutors">
+          <div className="tutor">
+            <div className="tutor-img">
+              <img src="./img/tutor-avatar.png" alt="tutor" width="150" />
+            </div>
+            <ul className="tutor-info">
+              <li>
+                <strong>Name Surname</strong>: John Doe
+              </li>
+              <li>
+                <strong>Phone</strong>: 0123 456 78 90
+              </li>
+              <li>
+                <strong>Mail</strong>: jdoe@mycourse.com
+              </li>
+              <li>
+                <strong>Office</strong>: A-215
+              </li>
+            </ul>
+          </div>
+          <div className="tutor">
+            <div className="tutor-img">
+              <img src="./img/tutor-avatar.png" alt="tutor" width="150" />
+            </div>
+            <ul className="tutor-info">
+              <li>
+                <strong>Name Surname</strong>: John Doe
+              </li>
+              <li>
+                <strong>Phone</strong>: 0123 456 78 90
+              </li>
+              <li>
+                <strong>Mail</strong>: jdoe@mycourse.com
+              </li>
+              <li>
+                <strong>Office</strong>: A-215
+              </li>
+            </ul>
+          </div>
+          <div className="tutor">
+            <div className="tutor-img">
+              <img src="./img/tutor-avatar.png" alt="tutor" width="150" />
+            </div>
+            <ul className="tutor-info">
+              <li>
+                <strong>Name Surname</strong>: John Doe
+              </li>
+              <li>
+                <strong>Phone</strong>: 0123 456 78 90
+              </li>
+              <li>
+                <strong>Mail</strong>: jdoe@mycourse.com
+              </li>
+              <li>
+                <strong>Office</strong>: A-215
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* GEOGRAPHY DEPARTMENT END */}
+      </div>
+    </div>
+  );
 };
 
 export default Tutors;
